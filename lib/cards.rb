f4204c8cdbeedf8c0152280062f5583a120bb7af
21hardermitchell <harder.62@osu.edu>
2024-02-03 13:10:51 -0500

    Fixed card and deck classes, initialized cards in the deck.

--- lib/cards.rb
+++ lib/cards.rb
@@ -1,6 +1,36 @@
 
 class Cards
-    COLOR = [:Red, :Green, :Purple]
-    NUMBER = [:One, :Two, :Three]
-    SHAPE = [:Triangle, :Oval, :Squiggle]
-    FILL = [:Empty, :Full, :Striped]
+
+     attr_reader :color, :number, :shape, :fill
+
+     def initialize(color, number, shape, fill)
+        @color = color
+        @number = number
+        @shape = shape
+        @fill = fill
+     end
+
+
+class Deck
+
+    def initialize 
+
+        @deck = []
+
+        colors = ["Red", "Green", "Purple"]
+        numbers = ["One", "Two", "Three"]
+        shapes = ["Diamond", "Oval", "Squiggle"]
+        fills = ["Empty", "Full", "Striped"]
+
+        colors.each do |color|
+            numbers.each do |number|
+                shapes.each do |shape|
+                    fills.each do |fill|
+                        card = Card.new(color, number, shape, fill)
+                        @deck << card
+                    end
+                end
+            end
+        end
+    end
+    
