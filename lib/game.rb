003e879ac65b1c9a1a89485afcf8c56ca0053303
21hardermitchell <harder.62@osu.edu>
2024-02-04 14:16:45 -0500

    Added game description.

--- lib/game.rb
+++ lib/game.rb
@@ -17,11 +17,27 @@ class Game
 
         @player1 = @players[0]
 
+        puts "Game Description:\n\n"
+        puts "Step 1: 12 'cards' will be dealt out to the first player.\n\n"
+        puts "Step 2: The first player will have 60 seconds to look for a set amongst the cards
+              and then type in the 3 card numbers associated with the card.\n\n"
+        puts "Step 3: If a selection was made, the computer will review the selection and determine 
+            if a set was found. If correct, the player who made the selection will recieve a point and
+            new cards will replace the selected ones. If incorrect, the computer will inform the player
+            of the result and prompt them for a new selection.\n\n"
+        puts "Step 4: Once the 60 seconds is up, an announcement will be made that the turn is up and it 
+            is now the second players turn.\n\n"
+        puts "Step 5: First player to get to 5 points will be crowned the victor!\n\n"
+        
+        
+
         puts "The game will now begin...#{@player1} will go first!\n\n"
 
         @deck_creator = Deck.new
         @deck_creator.shuffle
         @deck_creator.output_deck
+
+
         
     end
 
